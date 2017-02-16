c902c1e15233697027d925194c60834ceb6ea9bb
AsTex <v.milenko@innosoft.pro>
2017-02-16 16:49:35 +0300

    Initial Commit

---
+++ techs/zopfly.js
@@ -0,0 +1,37 @@
+/**
+ * enb-zopfli
+ * ========
+ *
+ * GZIPs files.
+ *
+ * **Options**
+ *
+ * * *String* **source** — Source file.
+ * * *String* **target** — Target file.
+ *
+ * **Example**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb-wrap/techs/wrap'));
+ * ```
+ */
+
+
+var fs = require('fs');
+var zopfli = require('node-zopfli');
+
+module.exports = require('enb/lib/build-flow').create()
+    .name('zopfli')
+    .target('target')
+    .useSourceFilename('source')
+    .defineRequiredOption('source')
+    .builder(function (fileName) {
+        console.log(fileName);
+        var node = this.node,
+            input = node.resolvePath(this._source),
+            output = node.resolvePath(this._target);
+        fs.createReadStream(input)
+            .pipe(zopfli.createGzip())
+            .pipe(fs.createWriteStream(output));
+    })
+    .createTech();
